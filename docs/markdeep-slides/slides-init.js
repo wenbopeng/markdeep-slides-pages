0b56db59503e650c6213c8a9b6965a0a0b98b40e
Wenbo Peng <pengwenbogood@126.com>
2025-12-18 14:40:35 +0800

    feat：slides-init.js

--- docs/markdeep-slides/slides-init.js
+++ docs/markdeep-slides/slides-init.js
@@ -3,7 +3,7 @@ document.write(`
 <script>
 markdeepSlidesOptions= {
     aspectRatio: 16 / 9,
-    theme: "markdeep-slides/themes/simple.css",
+    theme: "https://wenbopeng.github.io/markdeep-slides-pages/markdeep-slides/themes/simple.css",
     fontSize: 28,
     diagramZoom: 1.0,
     totalSlideNumber: true,
@@ -14,9 +14,9 @@ markdeepSlidesOptions= {
     modeChangeHook: (newMode) => {}
 };
 </script>
-<link rel="stylesheet" href="markdeep-slides/lib/markdeep-relative-sizes/1.11/relativize.css">
-<link rel="stylesheet" href="markdeep-slides/markdeep-slides.css">
-<script src="markdeep-slides/markdeep-slides.js"></script>
+<link rel="stylesheet" href="https://wenbopeng.github.io/markdeep-slides-pages/markdeep-slides/lib/markdeep-relative-sizes/1.11/relativize.css">
+<link rel="stylesheet" href="https://wenbopeng.github.io/markdeep-slides-pages/markdeep-slides/markdeep-slides.css">
+<script src="https://wenbopeng.github.io/markdeep-slides-pages/markdeep-slides/markdeep-slides.js"></script>
 
 <script>
     markdeepOptions = {
@@ -28,6 +28,6 @@ markdeepSlidesOptions= {
     };
 </script>
 <style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style>
-<script src="markdeep-slides/lib/markdeep/1.11/markdeep.min.js" charset="utf-8"></script>
+<script src="https://wenbopeng.github.io/markdeep-slides-pages/markdeep-slides/lib/markdeep/1.11/markdeep.min.js" charset="utf-8"></script>
 <script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>
 `);
